ce36931798d9ee64efec8653c75f344d041a6d39
Ryan Veach <rdveach@lcmail.lcsc.edu>
2020-04-10 12:10:07 -0700

    doesn't attempt to remove non-existant characters on disconnect

--- derpConnection.py
+++ derpConnection.py
@@ -109,8 +109,9 @@ async def client_connection(reader, writer):
             num_errors = 0
     
     finally:
-        derp.disconnect_character(character_name)
-        log.debug(f'Character "{character_name}" disconnected, closing connection')
+        if character_name != '':
+            derp.disconnect_character(character_name)
+            log.debug(f'Character "{character_name}" disconnected, closing connection')
         
 
 async def handle_CHARACTER(reader, writer):
